b2135516fb5e9a168d53d9214edde3e4f0a6d585
Erick Ramirez <erickramireztebalan@gmail.com>
2020-12-07 13:56:42 -0600

    feat: replace table for SQL

--- data/process_data.py
+++ data/process_data.py
@@ -56,7 +56,7 @@ def save_data(df, database_filename):
     """
     connection = 'sqlite:///{}'.format(database_filename)
     engine = create_engine(connection)
-    df.to_sql('disaster_message', engine, index=False)
+    df.to_sql('disaster_message', engine, index=False, if_exists = 'replace')
 
 
 def main():
